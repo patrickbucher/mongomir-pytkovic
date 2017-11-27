3b8f3f0d5f1ba71a8a6781c30907b46e87a922fa
christopherchristensen <christopher.christensen@live.com>
2017-11-27 16:36:34 +0100

    added index.html with styling

---
+++ webapp/scripts/datenbank.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+    $('.input').on('keyup', function(event) {
+        var currentInput = $(this).val();
+        // $.get(url('http://localhost:8000/match'), {
+        //
+        // });
+        console.log(currentInput);
+    });
+
+
+
+
+});
