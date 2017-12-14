327eed5059d26aa3a52790494efffff14fc69b43
chefe <hi@chefe.io>
2017-12-14 17:43:43 +0100

    Add api route to get stats

--- webapp/sample.py
+++ webapp/sample.py
@@ -8,20 +8,32 @@ import pprint
 
 client = MongoClient()
 mongo_db = client.soccer
+logger = logging.getLogger('Match')
+logger.setLevel(logging.DEBUG)
+logdir = os.environ['LOGDIR']
+logpath = os.path.join(logdir, 'falcon.log')
+fh = logging.FileHandler(logpath)
+logger.addHandler(fh)
 
 class MatchResource:
-    def __init__(self):
-        self.logger = logging.getLogger('Match')
-        self.logger.setLevel(logging.DEBUG)
-        logdir = os.environ['LOGDIR'] 
-        logpath = os.path.join(logdir, 'falcon.log')
-        fh = logging.FileHandler(logpath)
-        self.logger.addHandler(fh)
-
     def on_get(self, req, resp):
         """Handles GET requests"""
-        self.logger.debug('GET on /match')
-        resp.media = pprint.pformat(mongo_db.matches.find_one(), indent = 4)
+        logger.debug('GET on /api/match')
+        resp.media = pprint.pformat(mongo_db.matches.find_one())
+
+class StatsResource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        logger.debug('GET on /api/stats')
+        resp.media = pprint.pformat(list(mongo_db.matches.aggregate([{
+            "$group": {
+                "_id": "$league_id",
+                "total": {
+                    "$sum": 1
+                }
+            }
+        }])))
 
 api = falcon.API()
 api.add_route('/api/match', MatchResource())
+api.add_route('/api/stats', StatsResource())
