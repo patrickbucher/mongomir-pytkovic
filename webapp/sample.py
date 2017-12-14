bbe46bf87dfce1f8f8cc6ca653eb044617c48310
chefe <hi@chefe.io>
2017-12-14 18:33:39 +0100

    Implement find player matches

--- webapp/sample.py
+++ webapp/sample.py
@@ -19,7 +19,31 @@ class MatchResource:
     def on_get(self, req, resp):
         """Handles GET requests"""
         logger.debug('GET on /api/match')
-        resp.media = pprint.pformat(mongo_db.matches.find_one())
+        name = req.get_param('name')
+        resp.media = pprint.pformat(list(mongo_db.matches.aggregate([
+            {"$lookup": {
+                "from": "leagues",
+                "localField": "league_id",
+                "foreignField": "id",
+                "as": "matches_league"
+            }},
+            {"$project": {
+                "_id": 0,
+                "date": 1,
+                "date_timestamp": 1,
+                "matches_league.league": 1,
+                "matches_league.country": 1,
+                "round": 1,
+                "home_goals": 1,
+                "away_goals": 1,
+                "home_players": 1,
+                "away_players": 1
+            }},
+            {"$match": {
+                "$or": [{"home_players.name" : name}, {"away_players.name": name}]
+            }},
+            {"$sort": {"date_timestamp": -1}}
+        ])))
 
 class StatsResource:
     def on_get(self, req, resp):
