0786dafbbd18d8d7b0e9ab2e8f07cf5ee52e306a
chefe <hi@chefe.io>
2017-12-21 16:03:24 +0100

    Change ajax call to add data to result view

--- webapp/scripts/datenbank.js
+++ webapp/scripts/datenbank.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.search-btn').on('click', function(event) {
 
-        var input = $('.search-form').find('input').val();
+        var input = $('.search-form .input').val()
 
         $.ajax({
             url: 'http://localhost:8001/api/match?name='+input,
@@ -10,15 +10,38 @@ $(document).ready(function() {
             success: function (data) {
                 console.log('success');
                 console.log(data);
-                console.log(JSON.parse(data.replace(/'/g, '"')));
-
-                
-
+                $('.result-view').html('');
+                data.forEach(function (item) {
+                    $('.result-view').append(
+                            '<div class="result">' +
+                                '<div class="match-date">' +
+                                    '<h3>Match Date: ' + item.date + '</h3>' +
+                                '</div>' +
+                                '<div class="stats">' +
+                                    '<div class="team-home">' +
+                                        '<div class="team-home-name">' +
+                                            'Team Name' +
+                                        '</div>' +
+                                        '<div class="home">' +
+                                            item.home_team +
+                                        '</div>' +
+                                    '</div>' +
+                                    '<div class="team-away">' +
+                                        '<div class="team-away-name">' +
+                                            'Team Name' +
+                                        '</div>' +
+                                        '<div class="away">' +
+                                            item.away_team +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>');
+                });
+                console.log('done');
             },
             fail: function (data) {
                 console.log('error');
             }
         });
-        console.log('done');
     });
 });
